b011159d45bfe73ffc28377cff365a9e82027d6a
igorferreira007 <ferreiraigor007@gmail.com>
2023-01-23 20:33:08 -0300

    VendedorDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -12,6 +14,8 @@ public class Program {
 		Departamento departamento = new Departamento(1, "Livros");
 		
 		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departamento);
+		
+		VendedorDao vendedorDao = DaoFactory.criarVendedorDao();
 		
 		System.out.println(departamento);
 		System.out.println(vendedor);
---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.VendedorDao;
+import model.entities.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao {
+
+	@Override
+	public void inserir(Vendedor vendedor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Vendedor vendedor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor procurarPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> procurarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
